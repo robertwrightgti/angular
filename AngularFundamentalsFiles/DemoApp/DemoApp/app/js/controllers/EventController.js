b267764db7212efb011a9a56607fb76192053751
robw <robertjameswright@gmail.com>
2016-09-30 16:41:15 +0100

    custom service

--- AngularFundamentalsFiles/DemoApp/DemoApp/app/js/controllers/EventController.js
+++ AngularFundamentalsFiles/DemoApp/DemoApp/app/js/controllers/EventController.js
@@ -1,47 +1,10 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function ($scope) {
+    function ($scope, eventDataService) {
 
         $scope.sortOrder = "name";
-
-        $scope.event = {
-            name: "Angular boot camp",
-            date: "1 Aug 2016",
-            time: "12.30",
-            location: {
-                address: "Google headquarters",
-                city: "mountain View",
-                province: "CA"
-            },
-            imageUrl: "/img/angularjs-logo.png",
-            sessions: [
-                {
-                    name: "Starting stuff",
-                    creatorName: "Bob Fleming",
-                    duration: 1,
-                    level: "Beginner",
-                    abstract: "You know nothing.  You will LISTEN.",
-                    upVoteCount: 0
-                },
-                {
-                    name: "Getting better",
-                    creatorName: "Kenny Sansom",
-                    duration: 2,
-                    level: "Intermediate",
-                    abstract: "You think you know it - but you don't.",
-                    upVoteCount: 0
-                },
-                {
-                    name: "Rocking that shit",
-                    creatorName: "Iain Sharples",
-                    duration: 4,
-                    level: "Advanced",
-                    abstract: "I will crush your minds with code.",
-                    upVoteCount: 0
-                }
-            ]
-        };
+        $scope.event = eventDataService.event;
 
         $scope.upVoteSession = function(session) {
             session.upVoteCount++;
---
+++ AngularFundamentalsFiles/DemoApp/DemoApp/app/js/services/EventData.js
@@ -0,0 +1,41 @@
+eventsApp.factory('eventDataService', function() {
+   return {
+       event: {
+           name: "Angular boot camp",
+           date: "1 Aug 2016",
+           time: "12.30",
+           location: {
+               address: "Google headquarters",
+               city: "mountain View",
+               province: "CA"
+           },
+           imageUrl: "/img/angularjs-logo.png",
+           sessions: [
+               {
+                   name: "Starting stuff",
+                   creatorName: "Bob Fleming",
+                   duration: 1,
+                   level: "Beginner",
+                   abstract: "You know nothing.  You will LISTEN.",
+                   upVoteCount: 0
+               },
+               {
+                   name: "Getting better",
+                   creatorName: "Kenny Sansom",
+                   duration: 2,
+                   level: "Intermediate",
+                   abstract: "You think you know it - but you don't.",
+                   upVoteCount: 0
+               },
+               {
+                   name: "Rocking that shit",
+                   creatorName: "Iain Sharples",
+                   duration: 4,
+                   level: "Advanced",
+                   abstract: "I will crush your minds with code.",
+                   upVoteCount: 0
+               }
+           ]
+       }
+   };
+});
